7409834f133562f8e3ed92dac24eb52a8108cf29
kevinwiding <kevinwiding1@gmail.com>
2022-07-04 21:39:04 +0200

    tons of fixes

--- index.js
+++ index.js
@@ -3,53 +3,9 @@ MENU HOVER FUNC
 */
 
 function openMenu() {
-    document.body.classList += " menu--open"
+    document.querySelector("#landing").classList += " menu--open"
 }
 
 function closeMenu() {
-    document.body.classList.remove('menu--open')
-}
-
-/* 
-ITEM HOVER FUNC 
-*/
-
-function onHover1()
-{
-    $("#product1").attr('src', './assets/black-hoodie-1-back.jpg');
-}
-
-function offHover1()
-{
-    $("#product1").attr('src', './assets/black-hoodie-1-front.jpg');
-}
-
-function onHover2()
-{
-    $("#product2").attr('src', './assets/white-hoodie-1-back.jpg');
-}
-
-function offHover2()
-{
-    $("#product2").attr('src', './assets/white-hoodie-1-front.jpg');
-}
-
-function onHover3()
-{
-    $("#product3").attr('src', './assets/black-hoodie-2-back.jpg');
-}
-
-function offHover3()
-{
-    $("#product3").attr('src', './assets/black-hoodie-2-front.jpg');
-}
-
-function onHover4()
-{
-    $("#product4").attr('src', './assets/white-hoodie-2-back.jpg');
-}
-
-function offHover4()
-{
-    $("#product4").attr('src', './assets/white-hoodie-2-front.jpg');
+    document.querySelector("#landing").classList.remove('menu--open')
 }
